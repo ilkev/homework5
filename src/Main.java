06176e26bab0ec19f97113ba9e45149d3e75d227
ilyaka <ilyaka89@gmail.com>
2023-03-31 03:19:56 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Окно
+        JFrame window1 = new JFrame();
+        window1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window1.setVisible(true);
+        window1.setSize(600,600);
+        window1.getContentPane().setLayout((null)); //чтобы не растягивались элементы
+
+        //Первая кнопка
+        JButton button1 = new JButton();
+        button1.setText("iOS");
+        button1.setSize(100,100);
+        button1.setLocation(100,100);
+        ActionListener AListener1 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        };
+        button1.addActionListener(AListener1);
+        window1.add(button1);  //Добавляем кнопку
+
+
+        //Вторая кнопка
+        JButton button2 = new JButton();
+        button2.setText("Android");
+        button2.setSize(100,100);
+        button2.setLocation(500,100);
+
+        ActionListener AListener2 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        };
+        button2.addActionListener(AListener2);
+        window1.add(button2);
     }
 }
